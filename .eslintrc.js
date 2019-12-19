7721ad8563fa5b61d739528d62bbfc4070887c18
jgen16go <asai.work.usr@gmail.com>
2019-12-19 14:45:05 +0900

    update

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  root: true,
+  extends: ['eslint:recommended', 'plugin:import/errors', 'plugin:import/warnings'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: "module",
+    createDefaultProgram: true,
+  },
+  plugins: ['@typescript-eslint'],
+  settings: {
+    "import/resolver": {
+      "node": {
+        "extensions": [
+          ".js",
+          ".jsx",
+          ".json",
+          ".ts"
+        ]
+      }
+    }
+  },
+  rules: {
+    // typescript関係
+    'no-empty-function': 'off',
+    'no-unused-vars': 'off',
+    'no-useless-constructor': 'off',
+    'arrow-parens': 'off',
+    'import/extensions': ['off'],
+
+    // typescript無関係
+    'no-magic-numbers': 'off',
+    'no-restricted-syntax': 'off',
+    'no-param-reassign': 'off',
+    'import/prefer-default-export': 'off',
+    'import/export': 'off',
+    'import/no-useless-path-segments': 'error',
+    'import/no-cycle': 'error',
+    'no-mixed-operators': ['error', { allowSamePrecedence: true }],
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+}
